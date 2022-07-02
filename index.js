bb85d80d37f83421c0d9692c23a6e6538273e043
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-02 22:51:30 +0000

    July 2 - Maximum Units on a Truck. Exercise 1710

--- index.js
+++ index.js
@@ -2689,6 +2689,41 @@ var isSumEqual = function(firstWord, secondWord, targetWord) {
   return (parseInt(valueOfString(firstWord)) + parseInt(valueOfString(secondWord)))  === parseInt(valueOfString(targetWord))
 };
 
-log(isSumEqual("acb", "cba", "cdb")) // true
-log(isSumEqual("aaa", "a", "aab")) // false
-log(isSumEqual("aaa", "a", "aaaa")) // true
+//log(isSumEqual("acb", "cba", "cdb")) // true
+//log(isSumEqual("aaa", "a", "aab")) // false
+//log(isSumEqual("aaa", "a", "aaaa")) // true
+
+/*
+1710. Maximum Units on a Truck
+
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+    numberOfBoxesi is the number of boxes of type i.
+    numberOfUnitsPerBoxi is the number of units in each box of the type i.
+
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+*/
+
+var maximumUnits = function(boxTypes, truckSize) {
+  
+  boxTypes.sort((a, b) => b[1] - a[1]);
+  
+  let boxesTruck = 0;
+  
+  for (let [ boxes, units ] of boxTypes) {
+  
+  const takeBoxes = Math.min(boxes, truckSize);
+    
+  boxesTruck += units * takeBoxes;
+  truckSize -= takeBoxes;
+  if(!truckSize) break
+  }
+
+  return boxesTruck;
+};
+
+log(maximumUnits([[1,3],[2,2],[3,1]], 4)) // 8
+log(maximumUnits([[5,10],[2,5],[4,7],[3,9]], 10)) // 91
+log(maximumUnits([[1,3],[5,5],[2,5],[4,2],[4,1],[3,1],[2,2],[1,3],[2,5],[3,2]], 35)) // 76
