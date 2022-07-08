9a9fc9ac1b4bb95480e2ec54cc8538a4f9e514a6
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-08 02:27:26 +0000

    July 7 - Decode the Message. Exercise 2325

--- index.js
+++ index.js
@@ -2785,5 +2785,34 @@ var percentageLetter = function(s, letter) {
   return Math.floor((counter/s.length)*100)
 };
 
-log(percentageLetter("foobar", "o")) // 33
-log(percentageLetter("jjjj", "k")) // 0
+//log(percentageLetter("foobar", "o")) // 33
+//log(percentageLetter("jjjj", "k")) // 0
+
+/*
+2325. Decode the Message
+
+You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+    Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+    Align the substitution table with the regular English alphabet.
+    Each letter in message is then substituted using the table.
+    Spaces ' ' are transformed to themselves.
+
+    For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+
+Return the decoded message.
+*/
+
+var decodeMessage = function(key, message) {
+  key = Array.from(new Set(key.split(' ').join('')))
+  let decoded = ''
+
+  for(let char of message) {
+    decoded += char === ' ' ? ' ' : String.fromCharCode(key.indexOf(char) + 97)
+  }
+
+  return decoded
+};
+
+log(decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv")) // "this is a secret"
+log(decodeMessage("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb")) // "the five boxing wizards jump quickly"
