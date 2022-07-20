ad89528e03320cac76c296a60ef7bad1b24202de
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-20 23:30:18 +0000

    July 20 - Delete Node in a Linked List. Exercise 237

--- index.js
+++ index.js
@@ -3000,6 +3000,20 @@ var numberOfPairs = function(nums) {
   */
 };
 
-log(numberOfPairs([1,3,2,1,3,2,2])) // [3, 1]
-log(numberOfPairs([1,1])) // [1, 0]
-log(numberOfPairs([0])) // [0, 1]
+//log(numberOfPairs([1,3,2,1,3,2,2])) // [3, 1]
+//log(numberOfPairs([1,1])) // [1, 0]
+//log(numberOfPairs([0])) // [0, 1]
+
+/*
+237. Delete Node in a Linked List
+
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+*/
+
+var deleteNode = function(node) {
+  node.val = node.next.val
+  node.next = node.next.next
+};
+
