0c6307a646c4557b473d688c11ba02c0cc97ca29
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-08-02 01:22:09 +0000

    August 01 - Minimum Cost to Move Chips to The Same Position. Exercise 1217

--- index.js
+++ index.js
@@ -3177,6 +3177,34 @@ var smallestEqual = function(nums) {
   }
 };
 
-log(smallestEqual([0,1,2])) // 0
-log(smallestEqual([4,3,2,1])) // 2
-log(smallestEqual([1,2,3,4,5,6,7,8,9,0])) // -1
+//log(smallestEqual([0,1,2])) // 0
+//log(smallestEqual([4,3,2,1])) // 2
+//log(smallestEqual([1,2,3,4,5,6,7,8,9,0])) // -1
+
+/*
+1217. Minimum Cost to Move Chips to The Same Position
+
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+    position[i] + 2 or position[i] - 2 with cost = 0.
+    position[i] + 1 or position[i] - 1 with cost = 1.
+
+Return the minimum cost needed to move all the chips to the same position.
+*/
+
+var minCostToMoveChips = function(position) {
+  let evenCount = 0, oddCount = 0;
+
+  for (let pos of position) {
+    pos % 2 === 0 ? evenCount++ : oddCount++;
+  }
+
+  return Math.min(evenCount, oddCount)
+};
+
+log(minCostToMoveChips([1,2,3])) // 1
+log(minCostToMoveChips([2,2,2,3,3])) // 2
+log(minCostToMoveChips([1,1000000000])) // 1
+
