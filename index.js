23ba6ceeac1749060f5f2a1d8997c1a6e0f733f6
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-08-23 01:42:13 +0000

    August 22 - Sort Integers by The Number of 1 Bits. Exercise 1356

--- index.js
+++ index.js
@@ -3412,6 +3412,33 @@ var largestLocal = function(grid) {
   return result;
 };
 
-log(largestLocal([[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])) // [[9,9],[8,6]]
-log(largestLocal([[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]])) // [[2,2,2],[2,2,2],[2,2,2]]
+//log(largestLocal([[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])) // [[9,9],[8,6]]
+//log(largestLocal([[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]])) // [[2,2,2],[2,2,2],[2,2,2]]
 
+/*
+1356. Sort Integers by The Number of 1 Bits
+
+You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+Return the array after sorting it.
+*/
+
+var sortByBits = function(arr) {
+  const arrayObject = {};
+    
+  for (let element of arr) {
+    let counter = 0, item = element;
+        
+    while (item > 0) {
+			counter += (item & 1);    
+			item = (item >> 1);        
+    }
+        
+    arrayObject[element] = counter;
+  }
+
+  return arr.sort((a, b) => arrayObject[a] - arrayObject[b] || a - b) 
+};
+
+log(sortByBits([0,1,2,3,4,5,6,7,8])) // [0,1,2,4,8,3,5,6,7]
+log(sortByBits([1024,512,256,128,64,32,16,8,4,2,1])) // [1,2,4,8,16,32,64,128,256,512,1024]
